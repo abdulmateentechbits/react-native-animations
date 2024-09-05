fe48a1260409f528b6a5c335200e0fd5a0b4d1a8
abdulmateen <abdulmateen@sleepwellsystem.com>
2024-09-05 22:28:36 +0500

    refractor

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
+import ParallaxScrollAnimation from '@/components/animations/ParallaxScrollAnimation';
 import { StyleSheet, View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <SafeAreaView style={{flex:1}}>
+      <ParallaxScrollAnimation/>
+    </SafeAreaView>
   );
 }
 
